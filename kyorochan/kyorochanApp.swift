cdcb2dc636d2580663cda4506f41f0f24b75bdd9
Bill <bill092804@gmail.com>
2025-09-14 03:10:44 +0800

    Initial Commit

---
+++ kyorochan/kyorochanApp.swift
@@ -0,0 +1,17 @@
+//
+//  kyorochanApp.swift
+//  kyorochan
+//
+//  Created by 陳芸萱 on 2025/9/14.
+//
+
+import SwiftUI
+
+@main
+struct kyorochanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
